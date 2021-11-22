dccba23c7fec2175d2c464ac0630e81f4b10095a
Mark (Andy) Buck Jr <mabuckjr@gmail.com>
2021-11-21 21:12:32 -0800

    made things too complicated. Found a good way to classify things for ML

--- queries.sql
+++ queries.sql
@@ -38,7 +38,9 @@ CREATE TABLE commodity_table(
 	recycle INT NOT NULL,
 	total_yards INT NOT NULL,
 	needed_recycle INT NOT NULL,
+	enough_recycle VARCHAR NOT NULL,
 	needed_compost INT NOT NULL,
+	enough_compost VARCHAR NOT NULL,
 	PRIMARY KEY (cust)
 );
 
@@ -46,40 +48,6 @@ SELECT * FROM commodity_table;
 
 DROP TABLE commodity_table;
 
-
--- Create table for training data
-CREATE TABLE train_data(
-	cust INT NOT NULL,
-	biz_type VARCHAR NOT NULL,
-	compost INT NOT NULL,
-	garbage INT NOT NULL,
-	recycle INT NOT NULL,
-	total_yards INT NOT NULL,
-	needed_recycle INT NOT NULL,
-	needed_compost INT NOT NULL,
-	PRIMARY KEY (cust)
-);
-
-SELECT * FROM train_data;
-
-DROP TABLE train_data;
-
--- Create table for the testing data
-CREATE TABLE test_data(
-	cust INT NOT NULL,
-	biz_type VARCHAR NOT NULL,
-	compost INT NOT NULL,
-	garbage INT NOT NULL,
-	recycle INT NOT NULL,
-	total_yards INT NOT NULL,
-	needed_recycle INT NOT NULL,
-	needed_compost INT NOT NULL,
-	PRIMARY KEY (cust)
-);
-
-SELECT * FROM test_data;
-
-DROP TABLE test_data;
 
 -- Joining the two tables var tables (just in case I need it later)
 SELECT vc.cust,
@@ -112,7 +80,9 @@ SELECT vc.cust,
 	ct.recycle,
 	ct.total_yards,
 	ct.needed_recycle,
-	ct.needed_compost
+	ct.enough_recycle,
+	ct.needed_compost,
+	ct.enough_compost
 INTO full_table
 FROM var_cust as vc
 RIGHT JOIN var_serv as vs
@@ -124,47 +94,3 @@ SELECT * FROM full_table;
 
 Drop TABLE full_table;
 
-
---Join Var Cust, Var Serv, and train data (will have to remove duplicate rows in pandas)
-SELECT vc.cust,
-	vc.monthly_bill,
-	vs.tax_body,
-	tr.biz_type,
-	tr.compost,
-	tr.garbage,
-	tr.recycle,
-	tr.total_yards,
-	tr.needed_recycle,
-	tr.needed_compost
-INTO tr_table
-FROM var_cust as vc
-RIGHT JOIN var_serv as vs
-ON (vc.cust=vs.cust)
-INNER JOIN train_data as tr
-ON (vc.cust = tr.cust);
-
-SELECT * FROM tr_table;
-
-Drop TABLE tr_table;
-
---Join Var Cust, Var Serv, and test data (will have to remove duplicate rows in pandas)
-SELECT vc.cust,
-	vc.monthly_bill,
-	vs.tax_body,
-	te.biz_type,
-	te.compost,
-	te.garbage,
-	te.recycle,
-	te.total_yards,
-	te.needed_recycle,
-	te.needed_compost
-INTO te_table
-FROM var_cust as vc
-RIGHT JOIN var_serv as vs
-ON (vc.cust=vs.cust)
-INNER JOIN test_data as te
-ON (vc.cust = te.cust);
-
-SELECT * FROM te_table;
-
-Drop TABLE te_table;
